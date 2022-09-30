6640c07646e59e9c85d0699743dfe49fc8823aae
David Luna <Dluna.dev@gmail.com>
2022-09-29 22:32:25 -0300

    App de tareas

---
+++ src/classes/todo.class.js
@@ -0,0 +1,42 @@
+
+export class Todo {
+
+  static fromJSON({ id,tarea,completado,creado }) {
+
+    const tempTodo = new Todo( tarea );
+
+    tempTodo.id         = id;
+    tempTodo.completado = completado;
+    tempTodo.creado     = creado;
+
+    return tempTodo;
+  }
+
+
+  constructor( tarea ) {
+
+    this.tarea = tarea;
+
+    this.id         = new Date().getTime(); // 12843654
+    this.completado = false;
+    this.creado     = new Date();
+
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
